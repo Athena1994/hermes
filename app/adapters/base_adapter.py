79a9d72437b34a7cf3a6db0e42726c8c759330bb
Athena <samantha.zehrer@gmail.com>
2025-09-23 18:12:35 +0200

    added periods to fetch_ohcl

--- app/adapters/base_adapter.py
+++ app/adapters/base_adapter.py
@@ -1,9 +1,20 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 from typing import Iterable, Dict, Any, Iterator, Optional, Union
 from datetime import datetime
 
 from app.models import Symbol
 
+class Period(Enum):
+    TICK = 'tick'
+    ONE_MINUTE = '1m'
+    FIVE_MINUTES = '5m'
+    FIFTEEN_MINUTES = '15m'
+    ONE_HOUR = '1h'
+    FOUR_HOURS = '4h'
+    ONE_DAY = '1d'
+    ONE_WEEK = '1w'
+    ONE_MONTH = '1mo'
 
 class BaseAdapter(ABC):
     """Base adapter interface for data sources."""
@@ -14,7 +25,9 @@ class BaseAdapter(ABC):
 
     @abstractmethod
     def fetch_ohlc(self, symbol: str, 
-                   start: datetime = None, end: datetime = None) -> Iterator[dict[str, Any]]:
+                   period: Period | str,
+                   start: datetime = None, 
+                   end: datetime = None) -> Iterator[dict[str, Any]]:
         """Yield OHLC rows as dicts: {timestamp, open, high, low, close, volume} """
 
     
